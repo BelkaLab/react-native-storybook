f471decadf8f8052ef5ad2496eab30e6f223d5fd
Fedevvi <federica@belkadigital.com>
2023-06-05 10:00:09 +0200

    Add style, types and story

--- components/title/title.stories.tsx
+++ components/title/title.stories.tsx
@@ -1,9 +1,22 @@
 import { Title } from './title';
 import { ComponentMeta, ComponentStory } from '@storybook/react-native';
+import { TEXT_ORIENTATION } from './types';
 
 export default {
   title: 'Title',
-  component: Title
+  component: Title,
+  args: {
+    text: 'This is a title',
+    orientation: 'left'
+  },
+  argTypes: {
+    orientation: {
+      control: {
+        type: 'radio'
+      },
+      options: TEXT_ORIENTATION
+    }
+  }
 } as ComponentMeta<typeof Title>;
 
 export const Template: ComponentStory<typeof Title> = (args) => {
--- components/title/title.tsx
+++ components/title/title.tsx
@@ -1,7 +1,18 @@
-import { Text } from 'react-native';
+import { Text as RNText } from 'react-native';
+import styled from 'styled-components/native';
+import { TextOrientation } from './types';
 
-interface TitleProps {}
+interface TitleProps {
+  text: string;
+  orientation: TextOrientation;
+}
 
-export const Title = ({}: TitleProps): JSX.Element => {
-  return <Text>ciao</Text>;
+export const Title = ({ text, orientation }: TitleProps): JSX.Element => {
+  return <Text orientation={orientation}>{text}</Text>;
 };
+
+const Text = styled(RNText)<{ orientation: TextOrientation }>`
+  font-size: 20px;
+  font-weight: 500;
+  text-align: ${(props) => props.orientation};
+`;
