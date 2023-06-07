e7e6e9a4b40a1a6c73fea1e1cc1388def1e4e195
Fedevvi <federica@belkadigital.com>
2023-06-07 10:18:26 +0200

    Add prop and style

--- components/item/item.stories.tsx
+++ components/item/item.stories.tsx
@@ -3,7 +3,10 @@ import { ComponentMeta, ComponentStory } from '@storybook/react-native';
 
 export default {
   title: 'Item',
-  component: Item
+  component: Item,
+  args: {
+    text: 'This is an item'
+  }
 } as ComponentMeta<typeof Item>;
 
 export const Template: ComponentStory<typeof Item> = (args) => {
--- components/item/item.tsx
+++ components/item/item.tsx
@@ -1,7 +1,14 @@
-import { Text } from 'react-native';
+import { Text as RNText } from 'react-native';
+import styled from 'styled-components/native';
 
-interface ItemProps {}
+interface ItemProps {
+  text: string;
+}
 
-export const Item = ({}: ItemProps) => {
-  return <Text>Item</Text>;
+export const Item = ({ text }: ItemProps) => {
+  return <Text>{text}</Text>;
 };
+
+const Text = styled(RNText)`
+  font-size: 16px;
+`;
