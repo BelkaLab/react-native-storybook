b8142ac8f2ab220de276a2f06a6a3eba1ff27a2b
Fedevvi <federica@belkadigital.com>
2023-06-07 12:21:26 +0200

    Add react-native-svg, add checkbox icons

--- components/item/item.tsx
+++ components/item/item.tsx
@@ -1,12 +1,20 @@
 import { Text as RNText } from 'react-native';
 import styled from 'styled-components/native';
+import EmptyCheckBox from '../../icons/EmptyCheckBox';
+import CheckBox from '../../icons/CheckBox';
 
 interface ItemProps {
   text: string;
 }
 
 export const Item = ({ text }: ItemProps) => {
-  return <Text>{text}</Text>;
+  return (
+    <>
+      <EmptyCheckBox />
+      <CheckBox />
+      <Text>{text}</Text>
+    </>
+  );
 };
 
 const Text = styled(RNText)`
